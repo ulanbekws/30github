c6f531a352b7e9ed0af85c3878fc509e8c962d6e
Ulanbek <ulanbek.ws@gmail.com>
2025-02-22 20:39:33 +0600

    created profile migrations

--- core/models/post.py
+++ core/models/post.py
@@ -1,16 +1,14 @@
-from typing import TYPE_CHECKING
-
 from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
-
-if TYPE_CHECKING:
-    from .user import User
+from .mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_id_nullable = False
+    _user_id_unique = False
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(Text, default="", server_default="")
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    user: Mapped["User"] = relationship(back_populates="posts")
---
+++ core/models/profile.py
@@ -0,0 +1,18 @@
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from .mixins import UserRelationMixin
+
+
+class Profile(UserRelationMixin, Base):
+    _user_id_unique = True
+    _user_back_populates = "profile"
+
+    first_name: Mapped[str | None] = mapped_column(String(40))
+    last_name: Mapped[str | None] = mapped_column(String(40))
+    bio: Mapped[str | None]
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
+
+
